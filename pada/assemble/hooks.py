dc65f581481a5904b2b1884d57988ce3e950843b
eleveyuan <eleveyuan@gmail.com>
2022-02-27 09:29:23 +0800

    add register

--- pada/assemble/hooks.py
+++ pada/assemble/hooks.py
@@ -2,16 +2,33 @@ from typing import Callable
 
 import pandas as pd
 
+from pada.assemble import settings
+
 
 class Hooker:
 
-    def __init__(self, data: pd.DataFrame):
-        self._header = data.columns
+    def __init__(self):
         self._data_caller = None
+        self._features_caller = None
+        self._validation_caller = None
+        self._filter_caller = None
 
-    def register_feat_parse(self):
+    def register_data(self, call: Callable):
+        self._data_caller = call
         return self
 
-    def register_feat_valid(self):
-        # TODO
+    def register_features(self, call: Callable):
+        self._features_caller = call
         return self
+
+    def register_validation(self, call: Callable):
+        self._validation_caller = call
+        return self
+
+    def register_feature_selection(self, call: Callable):
+        self._filter_caller = call
+        return self
+
+    def handle(self):
+        _data_obj = self._data_caller(settings.DATA_CONF)
+        _features_obj = self._features_caller(settings.URLS_CONF)
